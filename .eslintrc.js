cacd6a849f89589d82261ae36a98206ce4f648ee
Max Stockner <mail@bluemaex.de>
2020-01-03 16:06:22 +0100

    Add import/extensions rule and alphabetize list

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,28 @@ module.exports = {
     'plugin:jest/style',
     'plugin:node/recommended',
     'plugin:promise/recommended',
-    'prettier',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/member-delimiter-style': ['warn', { multiline: { delimiter: 'none' } }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/member-delimiter-style': ['warn', { multiline: { delimiter: 'none' } }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': ['error', 'ignorePackages', {
+      'ts': 'never',
+      'tsx': 'never',
+      'js': 'never',
+      'jsx': 'never',
+      'mjs': 'never'
+    }],
     'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always'
+      },
+    ],
     'node/no-extraneous-import': 'off',
     'node/no-missing-import': 'off',
     'node/no-unpublished-require': 'off',
@@ -24,13 +37,6 @@ module.exports = {
     'class-methods-use-this': 'off',
     'func-names': 'off',
     'no-process-exit': 'warn',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-      },
-    ],
-    'import/newline-after-import': 'off',
   },
   env: {
     'jest/globals': true,
